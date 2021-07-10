29c891f1d6300f34611895921a1196eb8199052c
shalini <shalini.v@tvsnext.io>
2021-07-10 19:35:00 +0530

    [shalini]Stack

---
+++ StackQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------Stack Operations-------------");
+            Console.WriteLine("***1.Push operation***");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Stack stack = new Stack();
+            switch (num)
+            {
+                case 1:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
