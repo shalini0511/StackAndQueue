6b5549e059de44c0f125075d189c00e60113046d
shalini <shalini.v@tvsnext.io>
2021-07-10 19:53:33 +0530

    [shalini]Enqueue

--- StackQueue/Stack.cs
+++ StackQueue/Stack.cs
@@ -20,6 +20,36 @@ namespace StackQueue
             this.top = newNode;
             Console.WriteLine("New Node {0} is added ", newNode.data);
         }
+        //Peek method gives the top most element in stack without removing the element.
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Console.WriteLine("The element after peeking is : " + this.top.data);
+        }
+        //Pop Mthod-Removes the top most element
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Peek();
+            this.top = this.top.next;
+        }
+        //Checking the any underflow or overflow occurs
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
         public void Display()
         {
             Node temp = this.top;
@@ -34,5 +64,6 @@ namespace StackQueue
                 temp = temp.next;
             }
         }
+       
     }
 }
